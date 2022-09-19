5548ce01a20ab9aa2ed3c4158d30b2f79f8d40fa
Chloe Bates <bateschloe00@gmail.com>
2022-09-19 17:19:39 -0400

    added add cache form to dashboard

--- flask_app/controllers/caches.py
+++ flask_app/controllers/caches.py
@@ -10,3 +10,21 @@ def dashboard():
     if 'user_id' not in session:
         return redirect('/')
     return render_template('dashboard.html')
+
+@app.route('/newcache')
+def add_cache():
+    if 'user_id' not in session:
+        return redirect('/')
+    else:
+        return render_template('addcache.html')
+
+@app.route('/postcache', methods=["POST"])
+def post_new_cache():
+    data={
+        'latitude': request.form['latitude'],
+        'longitude': request.form['longitude'],
+        'description': request.form['description'],
+        'user_id': session['user_id']
+    }
+    Cache.save_cache(data)
+    return redirect ('/dashboard')
