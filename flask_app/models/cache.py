38b5b219670f7486c599985a231fd1711d90134a
Dalton Quesenberry <dq2505@gmail.com>
2022-09-19 17:38:43 -0700

    Minor fixes on passing data into some functions

--- flask_app/models/cache.py
+++ flask_app/models/cache.py
@@ -32,13 +32,13 @@ class Cache:
     # Input: User id 
     # Output: List of class objects with information
     @classmethod
-    def get_all_caches_by_user(cls):
+    def get_all_caches_by_user(cls, data):
         query = """
         SELECT * 
         FROM caches 
         WHERE user_id = %(id)s
         ;"""
-        results = connectToMySQL(cls.DB).query_db(query)
+        results = connectToMySQL(cls.DB).query_db(query, data)
         caches = []
         for cache in results:
             caches.append( cls(cache) )
@@ -47,13 +47,13 @@ class Cache:
     # Input: Cache id 
     # Output: Single class object
     @classmethod
-    def get_cache_by_id(cls):
+    def get_cache_by_id(cls, data):
         query = """
         SELECT * 
         FROM caches 
         WHERE id = %(id)s
         ;"""
-        results = connectToMySQL(cls.DB).query_db(query)
+        results = connectToMySQL(cls.DB).query_db(query, data)
         if len(results < 1):
             return False
         return cls(results[0])
@@ -61,7 +61,7 @@ class Cache:
     # Input: Cache id 
     # Output: Single class object w/ arrays of comments & users attached to comments
     @classmethod
-    def get_cache_by_id_with_comments(cls):
+    def get_cache_by_id_with_comments(cls, data):
         query = """
         SELECT * 
         FROM caches 
@@ -69,7 +69,7 @@ class Cache:
         LEFT JOIN users ON users.id = comments.user_id
         WHERE caches.id = %(id)s
         ;"""
-        results = connectToMySQL(cls.DB).query_db(query)
+        results = connectToMySQL(cls.DB).query_db(query, data)
         if len(results < 1):
             return False
         else:
